9f02dcd1eeb9651204db39a8ba947583a2497834
Yzer Berisha <yzerberisha@gmail.com>
2024-07-26 16:00:16 +0200

    Commiting Changes for lesson 6

--- Lesson_6/page-notitle.php
+++ Lesson_6/page-notitle.php
@@ -1,4 +1,10 @@
-<?php get_header(); ?>
+<?php 
+	
+/*
+	Template Name: Page No Title
+*/
+	
+get_header(); ?>
 
 	<?php 
 	
@@ -6,8 +12,8 @@
 		
 		while( have_posts() ): the_post(); ?>
 			
-			<h3><?php the_title(); ?></h3>
-			<div class="thumbnail-img"><?php the_post_thumbnail('large'); ?></div>
+			<h1>This is my Static Title</h1>
+			
 			<small>Posted on: <?php the_time('F j, Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(); ?></small>
 			
 			<p><?php the_content(); ?></p>
